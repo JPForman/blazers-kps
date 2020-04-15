d43bf3e8657e6ec2d4a27704cfe428f0e36e8539
Joshua Forman <cyborgforman@gmail.com>
2020-04-15 08:56:09 -0700

    setup kps component and gut premade css

---
+++ src/components/KeyPlayersStats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/keyPlayersStats.css'
+import testJsonObject from '../test_json_object.js';
+
+
+function KeyPlayersStats(){
+
+  const roster = testJsonObject
+  console.log(roster);
+
+  return(
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+}
+export default KeyPlayersStats;
