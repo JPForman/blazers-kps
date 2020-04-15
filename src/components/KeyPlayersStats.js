3cb589040a00e9f5daea3626f3fe4988acc33a8f
Joshua Forman <cyborgforman@gmail.com>
2020-04-15 15:18:44 -0700

    refactor to remove bootstrap and reactstrap

--- src/components/KeyPlayersStats.js
+++ src/components/KeyPlayersStats.js
@@ -6,7 +6,8 @@ import testJsonObject from '../test_json_object.js';
 function KeyPlayersStats(){
 
   const roster = testJsonObject;
-  const player = roster[8];
+  const playerOne = roster[1];
+  const playerTwo = roster[2];
 
   // link for full body image
   // https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png
@@ -15,27 +16,51 @@ function KeyPlayersStats(){
   // http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/1040x760/${player.id}.png
 
   return(
-    <div className='kpsWrapper container'>
+    <div className='kpsWrapper '>
       <h1 className='kpsTitle'>Key Players Stats - Home</h1>
-      <div className='playerCard row'>
-        <div className='playerInfo col-6'>
-          <p className='playerNumber'>{player.number}</p>
-          <p className='firstName'>{player.firstName}</p>
-          <p className='lastName'>{player.lastName}</p>
-          <div className='playerStats'>
-            <p>Points Per Game:  {player.pointsPerGame}</p>
-            <p>Rebounds Per Game: {player.reboundsPerGame}</p>
-            <p>Assits Per Game: {player.assistsPerGame}</p>
-            <p>Field Goal Percentage: {player.fieldGoalPercent}</p>
-            <p>Free Throw Percentage: {player.freeThrowPercent}</p>
-            <p>3 Point Percentage: {player.threePointFieldGoal}</p>
-            <p>Avg Steals Per Game: {player.stealsPerGame}</p>
-            <p>Avg Blocks Per Game: {player.blocksPerGame}</p>
+      <div className='cardRow'>
+        <div className='playerCard'>
+          <div className='playerInfo '>
+            <p className='playerNumber'>{playerOne.number}</p>
+            <p className='firstName'>{playerOne.firstName}</p>
+            <p className='lastName'>{playerOne.lastName}</p>
+            <div className='playerStats'>
+              <p>Points Per Game:  {playerOne.pointsPerGame}</p>
+              <p>Rebounds Per Game: {playerOne.reboundsPerGame}</p>
+              <p>Assits Per Game: {playerOne.assistsPerGame}</p>
+              <p>Field Goal Percentage: {playerOne.fieldGoalPercent}</p>
+              <p>Free Throw Percentage: {playerOne.freeThrowPercent}</p>
+              <p>3 Point Percentage: {playerOne.threePointFieldGoal}</p>
+              <p>Avg Steals Per Game: {playerOne.stealsPerGame}</p>
+              <p>Avg Blocks Per Game: {playerOne.blocksPerGame}</p>
+            </div>
+          </div>
+          <div className='imageWrapper '>
+            <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${playerOne.id}.png`} />
           </div>
         </div>
-        <div className='imageWrapper col-6'>
-          <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png`} />
-      </div>
+
+        <div className='playerCard'>
+          <div className='playerInfo '>
+            <p className='playerNumber'>{playerTwo.number}</p>
+            <p className='firstName'>{playerTwo.firstName}</p>
+            <p className='lastName'>{playerTwo.lastName}</p>
+            <div className='playerStats'>
+              <p>Points Per Game:  {playerTwo.pointsPerGame}</p>
+              <p>Rebounds Per Game: {playerTwo.reboundsPerGame}</p>
+              <p>Assits Per Game: {playerTwo.assistsPerGame}</p>
+              <p>Field Goal Percentage: {playerTwo.fieldGoalPercent}</p>
+              <p>Free Throw Percentage: {playerTwo.freeThrowPercent}</p>
+              <p>3 Point Percentage: {playerTwo.threePointFieldGoal}</p>
+              <p>Avg Steals Per Game: {playerTwo.stealsPerGame}</p>
+              <p>Avg Blocks Per Game: {playerTwo.blocksPerGame}</p>
+            </div>
+          </div>
+          <div className='imageWrapper '>
+            <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${playerTwo.id}.png`} />
+          </div>
+        </div>
+
       </div>
     </div>
   );
