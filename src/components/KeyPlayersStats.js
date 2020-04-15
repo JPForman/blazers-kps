0d6cadd40af2b86fcf419411e9cf7b4831b0bb04
Joshua Forman <cyborgforman@gmail.com>
2020-04-15 14:15:34 -0700

    style card

--- src/components/KeyPlayersStats.js
+++ src/components/KeyPlayersStats.js
@@ -6,7 +6,7 @@ import testJsonObject from '../test_json_object.js';
 function KeyPlayersStats(){
 
   const roster = testJsonObject;
-  const player = roster[1];
+  const player = roster[8];
 
   // link for full body image
   // https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png
@@ -15,23 +15,26 @@ function KeyPlayersStats(){
   // http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/1040x760/${player.id}.png
 
   return(
-    <div className='kpsWrapper'>
+    <div className='kpsWrapper container'>
       <h1 className='kpsTitle'>Key Players Stats - Home</h1>
       <div className='playerCard row'>
-        <div className='playerStats col-6'>
-          <p>{player.number}</p>
-          <p>{player.lastName} {player.firstName}</p>
-          <p>Points Per Game:  {player.pointsPerGame}</p>
-          <p>Rebounds Per Game: {player.reboundsPerGame}</p>
-          <p>Assits Per Game: {player.assistsPerGame}</p>
-          <p>Field Goal Percentage: {player.fieldGoalPercent}</p>
-          <p>Free Throw Percentage: {player.freeThrowPercent}</p>
-          <p>3 Point Field Goal Percentage: {player.threePointFieldGoal}</p>
-          <p>Avg Steals Per Game: {player.stealsPerGame}</p>
-          <p>Avg Blocks Per Game: {player.blocksPerGame}</p>
+        <div className='playerInfo col-6'>
+          <p className='playerNumber'>{player.number}</p>
+          <p className='firstName'>{player.firstName}</p>
+          <p className='lastName'>{player.lastName}</p>
+          <div className='playerStats'>
+            <p>Points Per Game:  {player.pointsPerGame}</p>
+            <p>Rebounds Per Game: {player.reboundsPerGame}</p>
+            <p>Assits Per Game: {player.assistsPerGame}</p>
+            <p>Field Goal Percentage: {player.fieldGoalPercent}</p>
+            <p>Free Throw Percentage: {player.freeThrowPercent}</p>
+            <p>3 Point Percentage: {player.threePointFieldGoal}</p>
+            <p>Avg Steals Per Game: {player.stealsPerGame}</p>
+            <p>Avg Blocks Per Game: {player.blocksPerGame}</p>
+          </div>
         </div>
         <div className='imageWrapper col-6'>
-          <img className='headShot' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png`} />
+          <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png`} />
       </div>
       </div>
     </div>
