0e5dd5c5640d8ea94bfdd414216dfbc1d4b7dd15
Joshua Forman <cyborgforman@gmail.com>
2020-04-17 14:54:05 -0700

    add styles to cards

--- src/components/KeyPlayersStats.js
+++ src/components/KeyPlayersStats.js
@@ -8,6 +8,7 @@ function KeyPlayersStats(){
   const roster = testJsonObject;
   const playerOne = roster[2];
   const playerTwo = roster[4];
+  const bigPlayers = [playerOne, playerTwo];
   const playerThree = roster[1];
   const playerFour = roster[3];
   const playerFive = roster[6];
@@ -26,59 +27,43 @@ function KeyPlayersStats(){
     <div className='kpsWrapper '>
       <h1 className='kpsTitle'>Key Players Stats - Home</h1>
       <div className='cardRow'>
-        <div className='kpsComponent'>
-          <div className='playerCard'>
-            <div className='playerInfo '>
-              <p className='playerNumber'>{playerOne.number}</p>
-              <p className='firstName'>{playerOne.firstName}</p>
-              <p className='lastName'>{playerOne.lastName}</p>
-              <div className='playerStats'>
-                <p>Points Per Game:  {playerOne.pointsPerGame}</p>
-                <p>Rebounds Per Game: {playerOne.reboundsPerGame}</p>
-                <p>Assits Per Game: {playerOne.assistsPerGame}</p>
-                <p>Field Goal Percentage: {playerOne.fieldGoalPercent}</p>
-              </div>
-            </div>
-            <div className='imageWrapper '>
-              <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${playerOne.id}.png`} />
-            </div>
-          </div>
-          <div className='miniRow'>
 
-            {miniPlayers.map((miniPlayer, index) => (
-              <div className='miniPlayerCard'>
-                <div>
-                  <p>{miniPlayer.firstName}</p>
-                  <p>{miniPlayer.lastName}</p>
-                </div>
-                <div className='miniPicWrapper'>
-                  <img className='miniPlayerPic' src={`http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/1040x760/${miniPlayer.id}.png`} />
+        {bigPlayers.map((playerOne, index) => (
+          <div className='kpsComponent'>
+            <div className='playerCard'>
+              <div className='playerInfo '>
+                <p className='playerNumber'>{playerOne.number}</p>
+                <p className='firstName'>{playerOne.firstName}</p>
+                <p className='lastName'>{playerOne.lastName}</p>
+                <div className='playerStats'>
+                  <p>Points Per Game:  {playerOne.pointsPerGame}</p>
+                  <p>Rebounds Per Game: {playerOne.reboundsPerGame}</p>
+                  <p>Assits Per Game: {playerOne.assistsPerGame}</p>
+                  <p>Field Goal Percentage: {playerOne.fieldGoalPercent}</p>
                 </div>
               </div>
-            ))}
-          </div>
-        </div>
-
-        <div className='kpsComponent'>
-          <div className='playerCard'>
-            <div className='playerInfo '>
-              <p className='playerNumber'>{playerTwo.number}</p>
-              <p className='firstName'>{playerTwo.firstName}</p>
-              <p className='lastName'>{playerTwo.lastName}</p>
-              <div className='playerStats'>
-                <p>Points Per Game:  {playerTwo.pointsPerGame}</p>
-                <p>Rebounds Per Game: {playerTwo.reboundsPerGame}</p>
-                <p>Assits Per Game: {playerTwo.assistsPerGame}</p>
-                <p>Field Goal Percentage: {playerTwo.fieldGoalPercent}</p>
+              <div className='imageWrapper '>
+                <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${playerOne.id}.png`} />
               </div>
             </div>
-            <div className='imageWrapper '>
-              <img className='playerPic' src={`https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${playerTwo.id}.png`} />
+            <div className='miniRow'>
+
+              {miniPlayers.map((miniPlayer, index) => (
+                <div className='miniPlayerCard'>
+                  <div className='miniPlayerStats'>
+                    <p>{miniPlayer.number}</p>
+                    <p>{miniPlayer.firstName}</p>
+                    <p>{miniPlayer.lastName}</p>
+                  </div>
+                  <div className='miniPicWrapper'>
+                    <img className='miniPlayerPic' src={`http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/1040x760/${miniPlayer.id}.png`} />
+                  </div>
+                </div>
+              ))}
+
             </div>
           </div>
-        </div>
-
-        
+        ))}
 
       </div>
     </div>
