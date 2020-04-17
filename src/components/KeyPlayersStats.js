d9c51fe39ec70fa6cd7ccfa942dc73a881ff3e0c
Joshua Forman <cyborgforman@gmail.com>
2020-04-16 17:17:11 -0700

    style ckps cards

--- src/components/KeyPlayersStats.js
+++ src/components/KeyPlayersStats.js
@@ -6,8 +6,10 @@ import testJsonObject from '../test_json_object.js';
 function KeyPlayersStats(){
 
   const roster = testJsonObject;
-  const playerOne = roster[1];
-  const playerTwo = roster[2];
+  const playerOne = roster[2];
+  const playerTwo = roster[4];
+  console.log(roster);
+  
 
   // link for full body image
   // https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/${player.id}.png
@@ -29,10 +31,6 @@ function KeyPlayersStats(){
               <p>Rebounds Per Game: {playerOne.reboundsPerGame}</p>
               <p>Assits Per Game: {playerOne.assistsPerGame}</p>
               <p>Field Goal Percentage: {playerOne.fieldGoalPercent}</p>
-              <p>Free Throw Percentage: {playerOne.freeThrowPercent}</p>
-              <p>3 Point Percentage: {playerOne.threePointFieldGoal}</p>
-              <p>Avg Steals Per Game: {playerOne.stealsPerGame}</p>
-              <p>Avg Blocks Per Game: {playerOne.blocksPerGame}</p>
             </div>
           </div>
           <div className='imageWrapper '>
@@ -50,10 +48,6 @@ function KeyPlayersStats(){
               <p>Rebounds Per Game: {playerTwo.reboundsPerGame}</p>
               <p>Assits Per Game: {playerTwo.assistsPerGame}</p>
               <p>Field Goal Percentage: {playerTwo.fieldGoalPercent}</p>
-              <p>Free Throw Percentage: {playerTwo.freeThrowPercent}</p>
-              <p>3 Point Percentage: {playerTwo.threePointFieldGoal}</p>
-              <p>Avg Steals Per Game: {playerTwo.stealsPerGame}</p>
-              <p>Avg Blocks Per Game: {playerTwo.blocksPerGame}</p>
             </div>
           </div>
           <div className='imageWrapper '>
